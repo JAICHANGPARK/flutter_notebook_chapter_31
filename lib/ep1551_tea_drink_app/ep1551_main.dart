8f76fbb443cc4f5e667f1070e5bc44824313fea6
JAICHANGPARK <aristojeff@gmail.com>
2023-06-18 10:36:12 +0900

    Update ep1551_main.dart

--- lib/ep1551_tea_drink_app/ep1551_main.dart
+++ lib/ep1551_tea_drink_app/ep1551_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_31/ep1551_tea_drink_app/view/tea_drink_main_page.dart';
 
 void main() {
-  
+  runApp(TeaDrinkApp());
 }
 
 class TeaDrinkApp extends StatelessWidget {
