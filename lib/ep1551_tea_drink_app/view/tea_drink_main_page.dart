e808850579625e61c7a76326f83971f44cd9588e
JAICHANGPARK <aristojeff@gmail.com>
2023-06-21 09:43:24 +0900

    Update tea_drink_main_page.dart

--- lib/ep1551_tea_drink_app/view/tea_drink_main_page.dart
+++ lib/ep1551_tea_drink_app/view/tea_drink_main_page.dart
@@ -27,56 +27,7 @@ class _TeaDrinkMainPageState extends State<TeaDrinkMainPage> {
                 children: [
                   TeaDrinkHomeWidget(),
                   Container(),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Order",
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(8),
-                          border: Border.all(
-                            color: Colors.grey[300]!,
-                          ),
-                        ),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 72,
-                        color: Colors.blue,
-                      ),
-                      Container(
-                        height: 72,
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        color: Colors.blue,
-                      ),
-                      const Text(
-                        "RECOMMENDED",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Expanded(
-                        child: ListView.separated(
-                          itemCount: 10,
-                          separatorBuilder: (context, _) => const Divider(),
-                          itemBuilder: (context, index) {
-                            return Container(
-                              height: 140,
-                              color: Colors.blue,
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  )
+
                 ],
               ),
             ),
