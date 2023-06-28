5389c3200dc527094242e4b14ea532e733fe3227
JAICHANGPARK <aristojeff@gmail.com>
2023-06-28 20:14:49 +0900

    Create ep1560_main.dart

---
+++ lib/ep1560_banking_app/ep1560_main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+
+}
+
+class BankingApp extends StatelessWidget {
+  const BankingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
