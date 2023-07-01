88949ff8a020a63fd69464031a00f4090f5fbc6b
JAICHANGPARK <aristojeff@gmail.com>
2023-07-01 09:21:03 +0900

    Update ep1563_main.dart

--- lib/ep1563_fashion_commerce_app/ep1563_main.dart
+++ lib/ep1563_fashion_commerce_app/ep1563_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_31/ep1563_fashion_commerce_app/view/fashion_main_page.dart';
 
 void main() {
-  runApp(FashionCommerceApp());
+  runApp(const FashionCommerceApp());
 }
 
 class FashionCommerceApp extends StatelessWidget {
@@ -10,7 +10,7 @@ class FashionCommerceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: FashionMainPage(),
     );
   }
