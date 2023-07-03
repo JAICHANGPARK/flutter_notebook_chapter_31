3b7018f09647e3403c75d219bdf21064abb5dee6
JAICHANGPARK <aristojeff@gmail.com>
2023-07-03 17:26:35 +0900

    Update fashion_detail_page.dart

--- lib/ep1563_fashion_commerce_app/view/fashion_detail_page.dart
+++ lib/ep1563_fashion_commerce_app/view/fashion_detail_page.dart
@@ -14,9 +14,31 @@ class _FashionDetailPageState extends State<FashionDetailPage> {
       body: Column(
         children: [
           Container(
-            height: 360,
+            height: 420,
             decoration: BoxDecoration(
               color: Colors.grey,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(24),
+                bottomLeft: Radius.circular(24),
+              ),
+            ),
+            padding: EdgeInsets.only(top: 48),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 24,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 24,
+                  )
+                ],
+              ),
             ),
           )
         ],
