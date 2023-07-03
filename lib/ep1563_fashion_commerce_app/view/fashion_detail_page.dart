c2928c97a39a6bbaceb500b53ddf72f43948d95f
JAICHANGPARK <aristojeff@gmail.com>
2023-07-03 17:30:27 +0900

    Update fashion_detail_page.dart

--- lib/ep1563_fashion_commerce_app/view/fashion_detail_page.dart
+++ lib/ep1563_fashion_commerce_app/view/fashion_detail_page.dart
@@ -40,6 +40,95 @@ class _FashionDetailPageState extends State<FashionDetailPage> {
                 ],
               ),
             ),
+          ),
+          Row(
+            children: [
+              Text("Title Title Title Title Title"),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.star),
+                    Text("4.9"),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Text("Size"),
+          SizedBox(
+            height: 32,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text("S"),
+                )
+              ],
+            ),
+          ),
+          Text("Color"),
+          Row(
+            children: [
+              Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.brown,
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.indigo,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ],
+          ),
+          Text("Description"),
+          Text("~~~~~~"),
+          Row(
+            children: [
+              Expanded(
+                  child: Column(
+                children: [
+                  Text("Price"),
+                  Text("\$2,299.00"),
+                ],
+              )),
+              Expanded(
+                  child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+              ))
+            ],
           )
         ],
       ),
