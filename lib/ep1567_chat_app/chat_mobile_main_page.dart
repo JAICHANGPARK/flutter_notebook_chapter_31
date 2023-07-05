2b36906bae084b4dcad9cc51b37228176cf8c0cd
JAICHANGPARK <aristojeff@gmail.com>
2023-07-05 18:41:32 +0900

    Update chat_mobile_main_page.dart

--- lib/ep1567_chat_app/chat_mobile_main_page.dart
+++ lib/ep1567_chat_app/chat_mobile_main_page.dart
@@ -10,15 +10,33 @@ class ChatMobileMainPage extends StatefulWidget {
 class _ChatMobileMainPageState extends State<ChatMobileMainPage> {
   @override
   Widget build(BuildContext context) {
+    int menuIndex = 0;
     return Scaffold(
       body: Column(
         children: [],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.chat_outlined),
             label: "Chat",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.workspaces),
+            label: "Story",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.groups),
+            label: "Group",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call),
+            label: "Call",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Setting",
           ),
         ],
       ),
