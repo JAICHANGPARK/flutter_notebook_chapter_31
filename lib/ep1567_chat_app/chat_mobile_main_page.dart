71a4afc0391b1f43b32bc2891e64755112e24c8d
JAICHANGPARK <aristojeff@gmail.com>
2023-07-05 18:36:23 +0900

    Create chat_mobile_main_page.dart

---
+++ lib/ep1567_chat_app/chat_mobile_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ChatMobileMainPage extends StatefulWidget {
+  const ChatMobileMainPage({super.key});
+
+  @override
+  State<ChatMobileMainPage> createState() => _ChatMobileMainPageState();
+}
+
+class _ChatMobileMainPageState extends State<ChatMobileMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
