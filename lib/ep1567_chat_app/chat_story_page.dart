7865551bf5ca98f46751079eb92ff1861c9a9122
JAICHANGPARK <aristojeff@gmail.com>
2023-07-07 18:27:49 +0900

    Update chat_story_page.dart

--- lib/ep1567_chat_app/chat_story_page.dart
+++ lib/ep1567_chat_app/chat_story_page.dart
@@ -11,7 +11,7 @@ class _ChatStoryPageState extends State<ChatStoryPage> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
           Row(
