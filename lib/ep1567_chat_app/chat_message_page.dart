dca1556bfbce78f96b64890438587d2214c09cbe
JAICHANGPARK <aristojeff@gmail.com>
2023-07-08 09:31:08 +0900

    Update chat_message_page.dart

---
+++ lib/ep1567_chat_app/chat_message_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatMessagePage extends StatefulWidget {
+  const ChatMessagePage({super.key});
+
+  @override
+  State<ChatMessagePage> createState() => _ChatMessagePageState();
+}
+
+class _ChatMessagePageState extends State<ChatMessagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
