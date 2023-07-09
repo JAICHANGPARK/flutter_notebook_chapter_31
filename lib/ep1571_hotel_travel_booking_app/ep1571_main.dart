9f11345f52fe9e6745df539bc1b4eca1b9a28489
JAICHANGPARK <aristojeff@gmail.com>
2023-07-09 10:17:31 +0900

    Create ep1571_main.dart

---
+++ lib/ep1571_hotel_travel_booking_app/ep1571_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HotelTravelBookingApp extends StatelessWidget {
+  const HotelTravelBookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
