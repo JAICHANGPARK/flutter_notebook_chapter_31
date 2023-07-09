039c7c013cd60b612a142b0c31b06004978bef39
JAICHANGPARK <aristojeff@gmail.com>
2023-07-09 10:19:06 +0900

    Create hotel_travel_main_page.dart

---
+++ lib/ep1571_hotel_travel_booking_app/view/hotel_travel_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HotelTravelMainPage extends StatefulWidget {
+  const HotelTravelMainPage({super.key});
+
+  @override
+  State<HotelTravelMainPage> createState() => _HotelTravelMainPageState();
+}
+
+class _HotelTravelMainPageState extends State<HotelTravelMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
