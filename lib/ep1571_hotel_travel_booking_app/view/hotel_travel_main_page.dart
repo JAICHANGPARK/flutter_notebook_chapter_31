0a622c16b60f9f2e60a1d0c986960e44a61eacdc
JAICHANGPARK <aristojeff@gmail.com>
2023-07-10 18:27:51 +0900

    Update hotel_travel_main_page.dart

--- lib/ep1571_hotel_travel_booking_app/view/hotel_travel_main_page.dart
+++ lib/ep1571_hotel_travel_booking_app/view/hotel_travel_main_page.dart
@@ -149,17 +149,47 @@ class _HotelTravelMainPageState extends State<HotelTravelMainPage> {
                 itemCount: 10,
                 itemBuilder: (context, index) {
                   return Container(
-                    width: 420,
+                    width: 340,
                     color: Colors.pink,
+                    margin: EdgeInsets.only(right: 16),
                     child: Column(
                       children: [
                         Expanded(child: Container()),
-                        Expanded(child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-
-                          ],
-                        )),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.orange,
+                                  ),
+                                  Text(
+                                    "5 Start Hotel",
+                                  ),
+                                  Text(
+                                    "37 reviews",
+                                  )
+                                ],
+                              ),
+                              Text(
+                                "Title, Title",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.location_on,
+                                    color: Colors.green,
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   );
