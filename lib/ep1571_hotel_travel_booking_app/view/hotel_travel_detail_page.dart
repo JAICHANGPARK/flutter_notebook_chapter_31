95bd2532f12a84228e1bf22556ea92542a004e5a
JAICHANGPARK <aristojeff@gmail.com>
2023-07-11 18:23:28 +0900

    Update hotel_travel_detail_page.dart

--- lib/ep1571_hotel_travel_booking_app/view/hotel_travel_detail_page.dart
+++ lib/ep1571_hotel_travel_booking_app/view/hotel_travel_detail_page.dart
@@ -10,6 +10,40 @@ class HotelTravelDetailPage extends StatefulWidget {
 class _HotelTravelDetailPageState extends State<HotelTravelDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back_ios)),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text("Hotels"),
+                    Row(
+                      children: [
+                        ...List.generate(
+                          5,
+                              (index) => Container(
+                            height: 8,
+                            width: 24,
+                            margin: EdgeInsets.symmetric(horizontal: 2),
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(8)
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
