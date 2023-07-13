36812141c907664e09bd6b635435d2029bee57f9
JAICHANGPARK <aristojeff@gmail.com>
2023-07-13 18:24:53 +0900

    Create ep1575_main.dart

---
+++ lib/ep1575_music_player_app/ep1575_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerApp extends StatelessWidget {
+  const MusicPlayerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
