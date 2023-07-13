064f38e7a074b99d930afdb0c4e89f26b504b1da
JAICHANGPARK <aristojeff@gmail.com>
2023-07-13 18:28:08 +0900

    Update ep1575_main.dart

--- lib/ep1575_music_player_app/ep1575_main.dart
+++ lib/ep1575_music_player_app/ep1575_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_31/ep1575_music_player_app/view/music_player_main_page.dart';
 
 void main() {
   runApp(MusicPlayerApp());
@@ -9,6 +10,8 @@ class MusicPlayerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MusicPlayerMainPage(),
+    );
   }
 }
