f5698fd223ed1c78ba0fc85ebc4308d4e38debc3
Dreamwalker <aristojeff@gmail.com>
2023-07-15 15:20:46 +0900

    Create music_player_search_page.dart

---
+++ lib/ep1575_music_player_app/view/music_player_search_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MusicPlayerSearchPage extends StatefulWidget {
+  const MusicPlayerSearchPage({super.key});
+
+  @override
+  State<MusicPlayerSearchPage> createState() => _MusicPlayerSearchPageState();
+}
+
+class _MusicPlayerSearchPageState extends State<MusicPlayerSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
