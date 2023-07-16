3639bc40c376eb5c23d04c8f500a75805c5d5fcd
JAICHANGPARK <aristojeff@gmail.com>
2023-07-16 10:15:30 +0900

    Update music_player_search_page.dart

--- lib/ep1575_music_player_app/view/music_player_search_page.dart
+++ lib/ep1575_music_player_app/view/music_player_search_page.dart
@@ -20,13 +20,64 @@ class _MusicPlayerSearchPageState extends State<MusicPlayerSearchPage> {
               color: Colors.grey[400]!,
             ),
           ),
-          margin: EdgeInsets.symmetric(horizontal: 16),
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
           padding: EdgeInsets.symmetric(horizontal: 8),
           child: TextField(
             decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: "What do you want to listen to?",
-              prefixIcon: Icon(Icons.search)
+                border: InputBorder.none, hintText: "What do you want to listen to?", prefixIcon: Icon(Icons.search)),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Followed Artist",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text("see all"),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: SizedBox(
+            height: 54 + 8 + 24,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.only(right: 16),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 28,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text("Dream"),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          
+          child: Container(
+            margin: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.grey,
             ),
           ),
         )
