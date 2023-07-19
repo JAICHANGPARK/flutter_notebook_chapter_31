36a6cf7a4f7734f3663a3ec2d6a1921870afbd2c
JAICHANGPARK <aristojeff@gmail.com>
2023-07-19 19:00:03 +0900

    Create ep1581_main.dart

---
+++ lib/ep1581_e_commerce_app_ui/ep1581_main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ECommerceAppUI());
+}
+
+class ECommerceAppUI extends StatefulWidget {
+  const ECommerceAppUI({super.key});
+
+  @override
+  State<ECommerceAppUI> createState() => _ECommerceAppUIState();
+}
+
+class _ECommerceAppUIState extends State<ECommerceAppUI> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
