70e43c8fbed25c48d1bca347ca8c440647a62028
JAICHANGPARK <aristojeff@gmail.com>
2023-07-22 07:53:20 +0900

    Update e_commerce_detail_page.dart

--- lib/ep1581_e_commerce_app_ui/view/e_commerce_detail_page.dart
+++ lib/ep1581_e_commerce_app_ui/view/e_commerce_detail_page.dart
@@ -93,6 +93,8 @@ class _ECommerceDetailPageState extends State<ECommerceDetailPage> {
                     ],
                   ),
                   Text("Product Details"),
+                  Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "),
                   Divider(),
                   Row(
                     children: [
