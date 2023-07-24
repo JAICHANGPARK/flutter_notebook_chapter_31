c619d24a80e1222bb3cad0b416f8338558b4cd6d
JAICHANGPARK <aristojeff@gmail.com>
2023-07-24 17:31:58 +0900

    Update ep1586_main.dart

--- lib/ep1586_apparel_app/ep1586_main.dart
+++ lib/ep1586_apparel_app/ep1586_main.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class ApparelApp extends StatelessWidget {
+  const ApparelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
