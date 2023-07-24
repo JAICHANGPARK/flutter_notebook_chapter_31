219a999324db7560ec6ead1fcfe3d682427be7c6
JAICHANGPARK <aristojeff@gmail.com>
2023-07-24 17:34:06 +0900

    Update apparel_login_page.dart

---
+++ lib/ep1586_apparel_app/view/apparel_login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ApparelLoginPage extends StatefulWidget {
+  const ApparelLoginPage({super.key});
+
+  @override
+  State<ApparelLoginPage> createState() => _ApparelLoginPageState();
+}
+
+class _ApparelLoginPageState extends State<ApparelLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold()
+  }
+}
