3ad7c216973fb60c95f9c1321e6ea3e01b4309a0
JAICHANGPARK <aristojeff@gmail.com>
2023-07-25 18:37:01 +0900

    Update apparel_home_page.dart

--- lib/ep1586_apparel_app/view/apparel_home_page.dart
+++ lib/ep1586_apparel_app/view/apparel_home_page.dart
@@ -16,41 +16,46 @@ class _ApparelHomePageState extends State<ApparelHomePage> {
       body: Column(
         children: [],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey,
-        onTap: (idx) {
-          setState(() {
-            _menuIndex = idx;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home_filled,
-              ),
-              label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.bolt_outlined,
-              ),
-              label: "Promo"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.favorite_border,
-              ),
-              label: "Favorite"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.shopping_bag_outlined,
-              ),
-              label: "Your Cart"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person_outline,
-              ),
-              label: "Profile"),
-        ],
+      bottomNavigationBar: SizedBox(
+        height: 72,
+        child: BottomNavigationBar(
+
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          currentIndex: _menuIndex,
+          onTap: (idx) {
+            setState(() {
+              _menuIndex = idx;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_filled,
+                ),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.bolt_outlined,
+                ),
+                label: "Promo"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite_border,
+                ),
+                label: "Favorite"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.shopping_bag_outlined,
+                ),
+                label: "Your Cart"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_outline,
+                ),
+                label: "Profile"),
+          ],
+        ),
       ),
     );
   }
