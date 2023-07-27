d463f5366c9d391fd0588e2c5049afa8d1deecfc
JAICHANGPARK <aristojeff@gmail.com>
2023-07-27 18:37:25 +0900

    Update apparel_detail_page.dart

---
+++ lib/ep1586_apparel_app/view/apparel_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ApparelDetailPage extends StatefulWidget {
+  const ApparelDetailPage({super.key});
+
+  @override
+  State<ApparelDetailPage> createState() => _ApparelDetailPageState();
+}
+
+class _ApparelDetailPageState extends State<ApparelDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
