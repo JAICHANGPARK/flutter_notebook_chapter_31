9b221a637d938f1a6ef83f7fa6bcea5d0e0b6063
JAICHANGPARK <aristojeff@gmail.com>
2023-07-28 18:36:15 +0900

    Update apparel_home_page.dart

--- lib/ep1586_apparel_app/view/apparel_home_page.dart
+++ lib/ep1586_apparel_app/view/apparel_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_31/ep1586_apparel_app/view/apparel_detail_page.dart';
+import 'package:flutter_notebook_chapter_31/ep1586_apparel_app/view/apperal_cart_page.dart';
 
 class ApparelHomePage extends StatefulWidget {
   const ApparelHomePage({super.key});
@@ -14,225 +15,234 @@ class _ApparelHomePageState extends State<ApparelHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: IndexedStack(
+        index: _menuIndex,
         children: [
-          Container(
-            height: 240,
-            // decoration: const BoxDecoration(
-            //   color: Colors.blue,
-            // ),
-            child: Stack(
-              children: [
-                Positioned(
-                  bottom: 30,
-                  left: 0,
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    padding: const EdgeInsets.all(4),
-                    color: Colors.brown[50],
-                    child: const Stack(
-                      children: [
-                        Positioned(
-                          left: 16,
-                          top: 72,
-                          bottom: 0,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "June Special Sale",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 6,
-                              ),
-                              Text("Get 60% discount during\nour special month!"),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  left: 16,
-                  right: 16,
-                  child: PhysicalModel(
-                    color: Colors.white,
-                    elevation: 4,
-                    borderRadius: BorderRadius.circular(4),
-                    child: Container(
-                      padding: const EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                            icon: Icon(Icons.search),
-                            border: InputBorder.none,
-                            hintText: "Search by categories, name, or brands.."),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16, top: 8, bottom: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Discover",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text("See All"),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 280,
-                    // decoration: const BoxDecoration(
-                    //   color: Colors.blue,
-                    // ),
-                    padding: const EdgeInsets.only(left: 16),
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => ApparelDetailPage(),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            width: 170,
-                            margin: const EdgeInsets.only(right: 8),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.orange,
-                                      borderRadius: BorderRadius.circular(4),
+          Column(
+            children: [
+              Container(
+                height: 240,
+                // decoration: const BoxDecoration(
+                //   color: Colors.blue,
+                // ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 30,
+                      left: 0,
+                      right: 0,
+                      top: 0,
+                      child: Container(
+                        padding: const EdgeInsets.all(4),
+                        color: Colors.brown[50],
+                        child: const Stack(
+                          children: [
+                            Positioned(
+                              left: 16,
+                              top: 72,
+                              bottom: 0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "June Special Sale",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24,
                                     ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 8,
-                                ),
-                                Column(
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text("Get 60% discount during\nour special month!"),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 16,
+                      right: 16,
+                      child: PhysicalModel(
+                        color: Colors.white,
+                        elevation: 4,
+                        borderRadius: BorderRadius.circular(4),
+                        child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.search),
+                                border: InputBorder.none,
+                                hintText: "Search by categories, name, or brands.."),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16, top: 8, bottom: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "Discover",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text("See All"),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 280,
+                        // decoration: const BoxDecoration(
+                        //   color: Colors.blue,
+                        // ),
+                        padding: const EdgeInsets.only(left: 16),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => ApparelDetailPage(),
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                width: 170,
+                                margin: const EdgeInsets.only(right: 8),
+                                child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    const Text(
-                                      "Flutter Shirt",
-                                    ),
-                                    const Text("Shirt"),
-                                    const Text(
-                                      "\$950.00",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
+                                    Expanded(
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.orange,
+                                          borderRadius: BorderRadius.circular(4),
+                                        ),
                                       ),
                                     ),
-                                    Row(
+                                    const SizedBox(
+                                      height: 8,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
-                                        const Text("\$1,500.00"),
-                                        Container(
-                                          child: const Text(
-                                            "-49%",
+                                        const Text(
+                                          "Flutter Shirt",
+                                        ),
+                                        const Text("Shirt"),
+                                        const Text(
+                                          "\$950.00",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
                                           ),
+                                        ),
+                                        Row(
+                                          children: [
+                                            const Text("\$1,500.00"),
+                                            Container(
+                                              child: const Text(
+                                                "-49%",
+                                              ),
+                                            )
+                                          ],
                                         )
                                       ],
                                     )
                                   ],
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16, top: 8, bottom: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Categories",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text("See All"),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 120,
-                    margin: EdgeInsets.only(left: 16),
-                    color: Colors.blue,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: 94,
-                          margin: EdgeInsets.only(right: 12),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(4),
-                                    color: Colors.green,
-                                  ),
                                 ),
                               ),
-                              SizedBox(
-                                height: 8,
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16, top: 8, bottom: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "Categories",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
                               ),
-                              Text("hoodies"),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text("See All"),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 120,
+                        margin: EdgeInsets.only(left: 16),
+                        color: Colors.blue,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              width: 94,
+                              margin: EdgeInsets.only(right: 12),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(4),
+                                        color: Colors.green,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("hoodies"),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          )
+                ),
+              )
+            ],
+          ),
+          Container(),
+          Container(),
+          ApparelCartPage(),
+          Container(),
         ],
       ),
       bottomNavigationBar: SizedBox(
