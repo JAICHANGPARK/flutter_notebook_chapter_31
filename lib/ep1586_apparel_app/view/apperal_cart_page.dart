c73ddd611172759b29bbc49ec6e32552230e99ca
JAICHANGPARK <aristojeff@gmail.com>
2023-07-28 18:38:30 +0900

    Update apperal_cart_page.dart

--- lib/ep1586_apparel_app/view/apperal_cart_page.dart
+++ lib/ep1586_apparel_app/view/apperal_cart_page.dart
@@ -10,21 +10,23 @@ class ApparelCartPage extends StatefulWidget {
 class _ApparelCartPageState extends State<ApparelCartPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppBar(
-          title: const Text("Your Cart"),
-          centerTitle: true,
-        ),
-        Expanded(child: Placeholder()),
-        Container(
-          height: 72,
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(4),
-          ),
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Cart"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(child: Placeholder()),
+          Container(
+            height: 72,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(4),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
