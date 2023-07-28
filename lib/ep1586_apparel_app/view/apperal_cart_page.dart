bd74f9bc2018b8fc3a382313f861b121493b4211
JAICHANGPARK <aristojeff@gmail.com>
2023-07-28 18:21:38 +0900

    Update apperal_cart_page.dart

--- lib/ep1586_apparel_app/view/apperal_cart_page.dart
+++ lib/ep1586_apparel_app/view/apperal_cart_page.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class ApparelCartPage extends StatefulWidget {
+  const ApparelCartPage({super.key});
+
+  @override
+  State<ApparelCartPage> createState() => _ApparelCartPageState();
+}
+
+class _ApparelCartPageState extends State<ApparelCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
