ac34b6de53292f95bfaf9c27cb666cb73fb8cc65
JAICHANGPARK <aristojeff@gmail.com>
2023-07-29 09:30:27 +0900

    Create split_bill_home_page.dart

---
+++ lib/ep1591_split_bill_app/view/split_bill_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SplitBillHomePage extends StatefulWidget {
+  const SplitBillHomePage({super.key});
+
+  @override
+  State<SplitBillHomePage> createState() => _SplitBillHomePageState();
+}
+
+class _SplitBillHomePageState extends State<SplitBillHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
