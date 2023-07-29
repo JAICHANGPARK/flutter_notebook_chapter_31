bd826137975e58e3f9c20a50fad87233cb13b249
JAICHANGPARK <aristojeff@gmail.com>
2023-07-29 09:33:43 +0900

    Update split_bill_home_page.dart

--- lib/ep1591_split_bill_app/view/split_bill_home_page.dart
+++ lib/ep1591_split_bill_app/view/split_bill_home_page.dart
@@ -19,9 +19,37 @@ class _SplitBillHomePageState extends State<SplitBillHomePage> {
               Row(
                 children: [
                   Container(
+                    padding: EdgeInsets.all(8),
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       border: Border.all(),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 4,
+                            ),
+                            SizedBox(width: 4,),
+                            CircleAvatar(
+                              radius: 4,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 4,),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 4,
+                            ),
+                            SizedBox(width: 4,),
+                            CircleAvatar(
+                              radius: 4,
+                            ),
+                          ],
+                        )
+                      ],
                     ),
                   )
                 ],
