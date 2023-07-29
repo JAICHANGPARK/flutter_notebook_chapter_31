aa891746b31f1b0934bca4d11ba50a2e178ddd6f
JAICHANGPARK <aristojeff@gmail.com>
2023-07-29 09:32:00 +0900

    Update split_bill_home_page.dart

--- lib/ep1591_split_bill_app/view/split_bill_home_page.dart
+++ lib/ep1591_split_bill_app/view/split_bill_home_page.dart
@@ -11,6 +11,17 @@ class SplitBillHomePage extends StatefulWidget {
 class _SplitBillHomePageState extends State<SplitBillHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(205, 198, 198, 1),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
