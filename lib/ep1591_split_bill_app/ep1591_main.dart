fa4fe29fd9866cef1b656ca5ebff7d674e5e160e
JAICHANGPARK <aristojeff@gmail.com>
2023-07-29 09:28:45 +0900

    Create ep1591_main.dart

---
+++ lib/ep1591_split_bill_app/ep1591_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class SplitBillApp extends StatelessWidget {
+  const SplitBillApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
