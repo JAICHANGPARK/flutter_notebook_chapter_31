a21a183bf479ec57f5cbe13337b8268d58ed82b5
JAICHANGPARK <aristojeff@gmail.com>
2023-07-29 09:34:10 +0900

    Update split_bill_home_page.dart

--- lib/ep1591_split_bill_app/view/split_bill_home_page.dart
+++ lib/ep1591_split_bill_app/view/split_bill_home_page.dart
@@ -16,43 +16,53 @@ class _SplitBillHomePageState extends State<SplitBillHomePage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(),
-                    ),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 4,
-                            ),
-                            SizedBox(width: 4,),
-                            CircleAvatar(
-                              radius: 4,
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 4,),
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 4,
-                            ),
-                            SizedBox(width: 4,),
-                            CircleAvatar(
-                              radius: 4,
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 4,
+                                backgroundColor: Colors.red,
+                              ),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              CircleAvatar(
+                                radius: 4,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 4,
+                              ),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              CircleAvatar(
+                                radius: 4,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
