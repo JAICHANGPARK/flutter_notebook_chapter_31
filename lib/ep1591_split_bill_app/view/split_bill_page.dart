f34a9701331214fc3075ce49ac14cadb2aef0f8c
JAICHANGPARK <aristojeff@gmail.com>
2023-08-01 21:36:02 +0900

    Update split_bill_page.dart

--- lib/ep1591_split_bill_app/view/split_bill_page.dart
+++ lib/ep1591_split_bill_app/view/split_bill_page.dart
@@ -11,23 +11,50 @@ class _SplitBillPageState extends State<SplitBillPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  shape: BoxShape.circle,
-                ),
-                child: IconButton(
-                  onPressed: () {}, icon: null,
-                  
-                ),
-              )
-            ],
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.arrow_back,
+                      ),
+                      splashRadius: 24,
+                      iconSize: 24,
+                      padding: EdgeInsets.all(0),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text("Split Bill"),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.person_2_outlined,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
