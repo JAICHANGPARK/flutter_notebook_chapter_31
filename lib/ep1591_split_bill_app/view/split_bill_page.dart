87a98149693e8017883c7199f6dbed45470c525d
JAICHANGPARK <aristojeff@gmail.com>
2023-08-01 21:30:11 +0900

    Update split_bill_page.dart

---
+++ lib/ep1591_split_bill_app/view/split_bill_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplitBillPage extends StatefulWidget {
+  const SplitBillPage({super.key});
+
+  @override
+  State<SplitBillPage> createState() => _SplitBillPageState();
+}
+
+class _SplitBillPageState extends State<SplitBillPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
