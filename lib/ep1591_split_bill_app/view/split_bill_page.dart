a92b2ce7c1da4f2e86a48168513e31ba71799ba3
JAICHANGPARK <aristojeff@gmail.com>
2023-08-02 20:48:03 +0900

    Update split_bill_page.dart

--- lib/ep1591_split_bill_app/view/split_bill_page.dart
+++ lib/ep1591_split_bill_app/view/split_bill_page.dart
@@ -219,12 +219,12 @@ class _SplitBillPageState extends State<SplitBillPage> {
                       children: [
                         Text("Lisa"),
                         SizedBox(
-                          height: 4,
+                          height:12,
                         ),
                         Expanded(
                           child: Container(
                             decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(32),
+                              borderRadius: BorderRadius.circular(64),
                               border: Border.all(),
                             ),
                           ),
