047ea07fcad2005d3f10c7478aefe9cd5c89c535
JAICHANGPARK <aristojeff@gmail.com>
2023-08-03 18:35:16 +0900

    Update ep1596_main.dart

--- lib/ep1596_student_app/ep1596_main.dart
+++ lib/ep1596_student_app/ep1596_main.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(StudentApp());
+}
+
+class StudentApp extends StatelessWidget {
+  const StudentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 
