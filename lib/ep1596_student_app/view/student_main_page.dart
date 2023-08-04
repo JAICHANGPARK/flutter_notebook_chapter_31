3e86f7b71fc3e6f2f568358293c1c2322b76c4ca
JAICHANGPARK <aristojeff@gmail.com>
2023-08-04 22:11:13 +0900

    Update student_main_page.dart

---
+++ lib/ep1596_student_app/view/student_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentMainPage extends StatefulWidget {
+  const StudentMainPage({super.key});
+
+  @override
+  State<StudentMainPage> createState() => _StudentMainPageState();
+}
+
+class _StudentMainPageState extends State<StudentMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
